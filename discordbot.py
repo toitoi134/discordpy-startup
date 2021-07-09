aee491064dba3b3bfba8bcbe7b48f9dffc13e3ff
toitoi134 <87202136+toitoi134@users.noreply.github.com>
2021-07-10 03:37:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,23 +18,37 @@ async def ping(ctx):
     await ctx.send('pong')
 
 
+import os
+import random
+import discord
+from discord.ext import commands
+from typing import Optional
+
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix=".", intents=intents)
+
+
 @bot.command()
-async def darts(ctx: commands.Context, channel_name: str):
+async def darts(ctx: commands.Context, channel_name: str, name: Optional[str] = None):
     if ctx.author.bot:
         return
-
     await ctx.message.delete()
 
     channel_name = channel_name.lstrip("#")
-    channels = ctx.guild.voice_channels  # ボイスチャンネルリストを取得
+    channels = ctx.guild.voice_channels
     for ch in channels:
         if ch.name == channel_name:
             if not ch.members:
                 await ctx.send(f"#{channel_name}には誰もいません")
                 return
             member = random.choice(ch.members)
-            await member.send("あなたが選ばれました！")
+            msg = f"{name}に選ばれました！" if name is not None else "あなたが選ばれました！"
+            await member.send(msg)
             return
 
     await ctx.send(f"#{channel_name}が存在しません")
-    
+
+
+TOKEN = os.environ.get("DISCORD_BOT_TOKEN")
+bot.run(TOKEN)
